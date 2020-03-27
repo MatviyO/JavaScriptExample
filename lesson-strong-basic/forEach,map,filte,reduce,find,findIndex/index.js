335f68fa605f7e6406bdec4880a7533822a32f69
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-03-27 20:29:50 +0200

    filte.map.reduce.foreach

---
+++ lesson-strong-basic/forEach,map,filte,reduce,find,findIndex/index.js
@@ -0,0 +1,39 @@
+const people = [
+    {name: 'oleh', age: 23, budget: 45646500},
+    {name: 'vin', age: 20, budget: 40345600},
+    {name: '1fdleh', age: 11, budget: 40534500},
+    {name: '2gfdgoleh', age: 2, budget: 534},
+    {name: '3gfdoleh', age: 43, budget: 405435300},
+    {name: '54foleh', age: 263, budget: 54300}
+]
+//
+// for (let i = 0; i < people.length; i++) {
+//     console.log(people[i])
+// }
+
+//for
+// for (let k of people) {
+//     console.log(k)
+// }
+//ForEach
+// people.forEach( d => {
+//    console.log(d)
+// })
+
+//map
+// const newPeeple = person.map( k => {
+//     console.log(k)
+// })
+//filter
+
+// const mass2 = people.filter(person => person.age >=18)
+// console.log(mass2)
+//
+
+//reduce
+// var arr = [1, 2, 3, 4, 5]
+// const mass = arr.reduce((total, next) => {
+//     console.log(total)
+//     return total + next
+// }, 0)
+// console.log(mass)
