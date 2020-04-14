e880b4e05907b2eb96293735cb8dc5c834d07078
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-15 00:11:38 +0300

    condition

--- lesson-strong-basic/bind,call,aply/index.js
+++ lesson-strong-basic/bind,call,aply/index.js
@@ -1,7 +1,34 @@
-let lin = null;
- let my = undefined
-if ( lin) {
-    console.log('true')
-} else {
-    console.log('false', typeof lin)
-}
+// let lin = null;
+//  let my = undefined
+// if ( lin) {
+//     console.log('true')
+// } else {
+//     console.log('false', typeof lin)
+// }
+// let year = '2014'
+//
+// if (year == 2015) {
+//    console.log('true')
+// } else {
+//     console.log('no')
+// }
+// let value = prompt('Какое "официальное" название JavaScript?', '');
+//
+// if (value == 'ECMAScript') {
+//     alert('Верно!');
+// } else {
+//     alert('Не знаете? ECMAScript!');
+// }
+// let currentUser = null;
+// let defaultUser = undefined;
+//
+// let name = currentUser || defaultUser || undefined;
+// console.log(name)
+// if ( currentUser || defaultUser) {
+//     console.log('true')
+// }
+let hour = 12;
+let minute = 30;
+
+if (hour == 11 && minute == 30) {
+    console.log('true')}
