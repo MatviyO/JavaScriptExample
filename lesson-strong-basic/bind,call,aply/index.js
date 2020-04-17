0a187cbc7a4b951994bf4029085284a0d147006d
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-17 18:37:21 +0300

    thiss

--- lesson-strong-basic/bind,call,aply/index.js
+++ lesson-strong-basic/bind,call,aply/index.js
@@ -19,6 +19,7 @@
 // } else {
 //     alert('Не знаете? ECMAScript!');
 // }
+////////////////////////////////////
 // let currentUser = null;
 // let defaultUser = undefined;
 //
@@ -33,7 +34,7 @@
 // if (hour == 11 && minute == 30) {
 //     console.log('true')}
 // let a = 2 + 2;
-//
+// /////////
 // switch (a) {
 //     case 3:
 //         console.log('1' );
@@ -48,18 +49,90 @@
 //         console.log('4' );
 // }
 // let a = 2 + 2;
-//
+//   /////////
 // switch (a) {
 //     case 3:
 //         console.log( '1' );
 //     case 4:
 //         console.log( '2!' );
-//     case 5:
+//     case 5:s
 //         console.log( '3' );
 //     default:
 //         console.log( '4' );
 // }
+///////////////////
 // let a = ''
 // if (!a){
 //     console.log('1')
 // } else { console.log('2')}
+////////////////////
+// let foo = {
+//     name: 'oleh',
+//     baz: function() {
+//         console.log(this);
+//     }
+// }
+// foo.baz();
+// let bar = foo.baz;
+// bar();
+///////////////////////
+// m = function () {
+//     this.x = 5;
+//     console.log(this.x);
+//     (function() {
+//         this.x = 3
+//         console.log(this.x)
+//     })();
+//     console.log(this.x)
+//     console.log(this)
+// }
+// const ob = {x: 2}
+// m()
+// console.log(this)
+///////////////////////////
+// var f = function() {
+//     this.x = 5;
+//     (function() {
+//         this.x = 3;
+//     })();
+//     console.log(this.x);
+// };
+//
+// var obj = {x: 4, m: function() {
+//         console.log(this.x);
+//     }};
+//
+// f();
+// new f();
+// obj.m();
+// new obj.m();
+// f.call(f);
+// obj.m.call(f);
+////////////////////////
+// function f() {
+//     this.x = 5;
+//     console.log(this)
+//     console.log(this === window); // false
+// }
+// var o = new f();
+// console.log(o.x === 5); // true
+///////////////////////
+// var o = {
+//     f: function() {
+//         console.log(this)
+//         return this;
+//     }
+// }
+// console.log(o.f() === o); // true
+//////////////
+var bar = "bar2";
+
+function daz(){
+    var bar = "bar5";
+    (function maz(){
+
+        console.log(this.bar);
+    })()
+    // maz();
+}
+daz();  // bar2
