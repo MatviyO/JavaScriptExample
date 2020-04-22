413d93fbbac5f54e90ad8eedbd743326c108d20c
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-22 21:03:04 +0300

    array

---
+++ lesson-strong-basic/array/index.js
@@ -0,0 +1,22 @@
+// function came(str) {
+//     let s = str.split('-')
+//     let b = s[1].toUpperCase()
+//     s[1] = b
+//     console.log(s)
+//     let k = s.join('')
+//     console.log(k)
+//     return k
+// }
+// console.log(came("background-color"))
+
+//2
+// function fil(arr, a, b) {
+//     return arr.filter(item => a <= item && item <=b )
+// }
+//
+// let arr = [2, 6,4 ,9]
+// console.log(fil(arr, 1, 4))
+//3
+// let arr = [5, 2, 1, -10, 8];
+//
+// arr.sort((a,b) => b -a)
