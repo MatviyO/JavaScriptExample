92d72091254f18b7bb59578e9cf99323fbab77a7
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-24 18:41:32 +0300

    stru

--- lesson-strong-basic/recursion/index.js
+++ lesson-strong-basic/recursion/index.js
@@ -1,30 +1,74 @@
-// function calcValues(a, b) {
-//     return [
-//         a + b, a - b, a * b, a / b
-//     ]
+// let company = { // тот же самый объект, сжатый для краткости
+//     sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600 }],
+//     development: {
+//         sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+//         internals: [{name: 'Jack', salary: 1300}]
+//     }
+// };
+//
+// // Функция для подсчёта суммы зарплат
+// function sumSalaries(department) {
+//     if (Array.isArray(department)) { // случай (1)
+//         console.log('if', department)
+//         return department.reduce((prev, current) => prev + current.salary, 0); // сумма элементов массива
+//     } else { // случай (2)
+//         let sum = 0;
+//         for (let subdep of Object.values(department)) {
+//             console.log(subdep)
+//             sum += sumSalaries(subdep); // рекурсивно вызывается для подотделов, суммируя результаты
+//         }
+//         console.log(sum, 'else')
+//         return sum;
+//     }
 // }
-// const [sum,sub] = calcValues(42, 10)
-// const [sum,,mult, ...other] = calcValues(42, 10)
-// const sum = result[0]
-// const sub = result[1]
-// const [sub, sub] = result
+//
+// alert(sumSalaries(company)); // 6700
+//2
+// function cum(n) {
+//     for (let i = 0; i <=n; i++) {
+//         sum += i
+//     }
+//     return sum
+// }
+// console.log(cum(100))
+//
+// function suc(n) {
+//     if (n == 1) return 1
+//     return n + suc(n - 1)
+// }
+//3
+// function fac(n) {
+//     return (n != 1) ? n * fac(n -1) : 1
+// }
+// console.log(fac(5))
+//fibonachi
+// let person = {
+//     name: "John",
+//     sayHi: function() {
+//     console.log("Hi, ", this.name);}
+// }
+// console.log(person.sayHi())
+//zamekanie
 
-// console.log(sum, sub)
-// objects
-const person = {
-    name: 'MAx',
-    age: 20,
-    address: {
-        country: 'Ukrain',
-        city: 'kiev'
-    }
-}
-// const {name, age, address: {country, city}} = person
-// console.log(name,age,country, city)
-// const p = {...person}
-// console.log(p.name)
-// console.log(person)
-function logPerson({name, age}) {
-    console.log(name + '' + agee)
-}
-logPerson(person)
+// function f() {
+//     let count = 0;
+//     return function () {
+//         return count++;
+//     }
+// }
+// let counter = f();
+// console.log(counter())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
