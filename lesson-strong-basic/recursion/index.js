9bf08596b77e209b24a035bfa979715b667a14fb
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-24 19:45:10 +0300

    globalLexicalEnvironment

--- lesson-strong-basic/recursion/index.js
+++ lesson-strong-basic/recursion/index.js
@@ -59,9 +59,18 @@
 // let counter = f();
 // console.log(counter())
 
-
-
-
+function getCounter() {
+    let counter = 0;
+    return function() {
+        return counter++;
+    }
+}
+let count = getCounter();
+let count1 = getCounter();
+console.log(count());  // 0
+console.log(count());  // 1
+console.log(count1());  // 1
+console.log(count());  // 2
 
 
 
