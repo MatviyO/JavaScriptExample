b32de0a3df416dc59df7afd0e0a7b05783d42557
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-25 14:22:09 +0300

    up

--- lesson-strong-basic/call,aply/index.js
+++ lesson-strong-basic/call,aply/index.js
@@ -11,16 +11,39 @@
 //
 // f1000("test"); // показывает "test" после 1000 мс
 //2
-function debounce(f, ms) {
-    let isColumn = false;
-    return function() {
-        if (isColumn) return;
-        f.apply(this, arguments)
-        isColumn = true;
-        setTimeout(() =>  isColumn = false, ms)
-    }
-}
-let f = debounce(alert, 1000);
+// function debounce(f, ms) {
+//     let isColumn = false;
+//     return function() {
+//         if (isColumn) return;
+//         f.apply(this, arguments)
+//         isColumn = true;
+//         setTimeout(() =>  isColumn = false, ms)
+//     }
+// }
+// let f = debounce(alert, 1000);
+//
+// f(1); // выполняется немедленно
+// f(2); // проигнорирован
+//this
+// let user = {
+//     firstName: "Вася",
+//     sayHi() {
+//         alert(`Привет, ${this.firstName}!`);
+//     }
+// };
+// let sayHi = user.sayHi.bind(user); // (*)
+// sayHi(); // Привет, Вася!
+// setTimeout(sayHi, 1000); // Привет, Вася!
+//1
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
 
-f(1); // выполняется немедленно
-f(2); // проигнорирован
+    showList() {
+        this.students.forEach(
+            student => alert(this.title + ': ' + student)
+        );
+    }
+};
+
+group.showList();
