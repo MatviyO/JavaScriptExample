4f81d2fc4ad4178c54ffa443430542eb5eb38db2
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-04-30 18:56:16 +0300

    classClock

--- lesson-strong-basic/class/index.js
+++ lesson-strong-basic/class/index.js
@@ -1,86 +1,38 @@
-// const animal = {
-//     name: 'animal',
-//     age: 5,
-//     hasTail: true
-// }
-
-//
-// class Animal {
-//     static type = 'ANIMAL'
-//     constructor(options) {
-//         this.name = options.name
-//         this.age = options.age
-//         this.hasTail =  options.hasTail
-//     }
-//     voice() {
-//         console.log('i am voice')
-//     }
-// }
-// const animal = new Animal({
-//     name: 'Animal',
-//     age: 5,
-//     hasTail: true
-// })
-//
-// class Cat extends Animal {
-//     static type = 'Cat'
-//     constructor(options) {
-//         super(options);
-//         this.color = options.color
-//     }
-//     voice() {
-//         super.voice()
-//         console.log('i am cat voice')
-//     }
-//     get ageInfo() {
-//         return this.age * 7
-//     }
-//     set ageIfo(newAge) {
-//         this.age = newAge
-//     }
-//
-// }
-//
-// const cat = new Cat({
-//     name: 'Cat',
-//     age: 7,
-//     hasTail: true,
-//     color: 'black'
-// })
-
-
-
-
-//  example teotry
-
-class Component {
-    constructor(selector) {
-    this.$el = document.querySelector(selector)
+class Clock {
+    constructor({ template }) {
+        this.template = template;
     }
-    hide() {
-        this.$el.style.display = 'none'
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
     }
-    show() {
-        this.$el.style.display = 'block'
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
     }
 }
 
-class Box extends Component {
-    constructor(options) {
-        super(options.selector);
-        this.$el.style.width = this.$el.style.height = options.size + 'px'
-        this.$el.style.background = options.color
-    }
-}
 
-const box1 = new Box({
-    selector: '#box1',
-    size: 100,
-    color: 'red'
-})
-
-
-
-
-
-
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
