7120a11bc8e4af725c1fb9a056602c022e7e9e49
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-04 22:03:53 +0300

    genc

--- ts/src/generic.ts
+++ ts/src/generic.ts
@@ -29,3 +29,68 @@
 //     }
 // }
 // console.log(withCount('jkfdkdfdf dfodfo'))
+
+
+
+// function getObjectValue<T extends object, R extends keyof T>(obj: T, key: R) {
+//     return obj[key]
+// }
+// const person = { name: 'jon', age: 23}
+// console.log(getObjectValue(person, 'name'))
+// console.log(getObjectValue(person, 'age'))
+// console.log(getObjectValue(person, 'job'))
+
+
+//
+// class Collection<T> {
+//
+//     constructor(private _items: T[] = []) {
+//
+//     }
+//
+//
+//     add(item: T) {
+//         this._items.push(item)
+//     }
+//     remove(item: T) {
+//         this._items = this._items.filter( c => c ! == item)
+//     }
+//     get items(): T[] {
+//         return this._items
+//     }
+// }
+// const strings = new Collection<string>(['one','two','thre'])
+// strings.add('!')
+// strings.remove('two')
+// console.log(strings.items)
+
+
+///============
+
+interface Car {
+    model: string
+    year: number
+}
+
+function createCar(model: string, year: number): Car {
+    const car: Partial<Car> = {}
+
+    if ( model.length > 3) {
+        car.model = model
+    }
+
+    if (year > 2000) {
+        car.year = year
+    }
+    return car as Car
+}
+
+//===
+
+const cars: Array<string> = ['ford', 'audi']
+
+
+
+
+
+
