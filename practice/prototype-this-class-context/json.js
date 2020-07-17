9a8e13d6c7a54e593c0cb2f252d0bc02d5898360
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-07-17 11:41:48 +0300

    s

--- practice/prototype-this-class-context/json.js
+++ practice/prototype-this-class-context/json.js
@@ -1,6 +1,33 @@
-let user = {
-    name: "Василий Иванович",
-    age: 35
+// let user = {
+//     name: "Василий Иванович",
+//     age: 35
+// };
+//
+// let user2 = user.json();
+// console.log(user2)
+
+// let student = {
+//     name: 'John',
+//     age: 30,
+//     isAdmin: false,
+//     courses: ['html', 'css', 'js'],
+//     wife: null
+// };
+//
+// let json = JSON.stringify(student);
+//
+// alert(typeof json); // мы получили строку!
+//
+// alert(json);
+let room = {
+    number: 23
 };
 
-let user2 = JSON.parse(JSON.stringify(user));
+let meetup = {
+    title: "Conference",
+    participants: [{name: "John"}, {name: "Alice"}],
+};
+
+room.occupiedBy = meetup; // room ссылается на meetup
+
+console.log( JSON.stringify(meetup) );
